3430cad69413632a01649128c5a369e8a566cc82
AJlEKCAHDP_lV <alex.rom23@mail.ru>
2023-01-31 11:39:41 +0300

    refactor example

--- example/src/main.cc
+++ example/src/main.cc
@@ -5,11 +5,11 @@
 
 using scal = typename red_engine::scalar<double>;
 int main() {
-  std::vector <scal::pointer> x {scal::create(1.0),
-  scal::create(-2.0)};
+  std::vector<scal::pointer> x{scal::create(1.0), scal::create(-2.0)};
+
   auto n = nn::MLP<double>{2, {2, 1}};
   auto tmp = n(x);
   auto r = std::get<1>(tmp);
   r->backprop();
-  r->draw_dot("gout.dot");
+  r->draw_dot("dump.dot");
 }
